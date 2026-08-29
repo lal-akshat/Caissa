cc860958dc7512811ce429f544030143e4431e8b
Akshat Lal <akshat.lal13@gmail.com>
2026-08-28 23:58:37 -0400

    Minor formatting changes

--- Caissa/Chess Engine/PossibleMoves.cs
+++ Caissa/Chess Engine/PossibleMoves.cs
@@ -196,11 +196,9 @@ public class PossibleMoves
 
         for (int i = 0; i < offsets.GetLength(0); i++)
         {
-            int xMove =
-                xPos + offsets[i, 0];
+            int xMove = xPos + offsets[i, 0];
 
-            int yMove =
-                yPos + offsets[i, 1];
+            int yMove = yPos + offsets[i, 1];
 
             if (IsLegalKnightMove(
                     board,
