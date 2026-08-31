80fc3af7d8858be9eaf7f9a63131f96e0dfe7e7b
Akshat Lal <akshat.lal13@gmail.com>
2026-08-31 01:33:48 -0400

    Program.cs update

--- Caissa/Program.cs
+++ Caissa/Program.cs
@@ -6,7 +6,7 @@ static class Program
     /// The main entry point for the application.
     /// </summary>
     [STAThread]
-    static void Main()
+    private static void Main()
     {
         ApplicationConfiguration.Initialize();
 
