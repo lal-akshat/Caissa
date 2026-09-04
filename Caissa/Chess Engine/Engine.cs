6d448c92bb0512f1f7057f32040fbd2a7c7da83f
Akshat Lal <akshat.lal13@gmail.com>
2026-09-04 02:59:17 -0400

    Engine.cs comments and bug fixes

--- Caissa/Chess Engine/Engine.cs
+++ Caissa/Chess Engine/Engine.cs
@@ -4,18 +4,22 @@ namespace Caissa.Chess_Engine;
 
 public class Engine
 {
+    // Store the search object used to actually find the best move
     private readonly Search _search;
 
     public Engine()
     {
+        // Creates a new search object and stores it in _search
         _search = new Search();
     }
 
+    // Finds the best move given the board and player
     public Move? FindBestMove(
         Board board,
         PieceColor color,
         int depth)
     {
+        // Returns the best move for the given board, color, and depth
         return _search.FindBestMove(board, color, depth);
     }
 }
